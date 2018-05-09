941f793f5a521ddf4e461089a5bcb62a104ff14b
Ubuntu <ubuntu@ip-172-31-22-145.ec2.internal>
2018-05-09 17:01:09 +0000

    finishing project

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 
 
@@ -12,6 +13,9 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    for i in range(0, len(series) - window_size):
+        X.append(series[i:i + window_size])
+        y.append(series[i + window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -23,13 +27,21 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    model = Sequential()
+    model.add(keras.layers.LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))    
+    return model
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    import string
+    lowercase = string.ascii_lowercase + ' '
+    l = list(text)
+    for i in range(0, len(l)):
+        if l[i] not in lowercase and l[i] not in punctuation:
+            l[i] = ''
+    text = "".join(l)
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +49,16 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(keras.layers.LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    return model
